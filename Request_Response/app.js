ee748751d488429c45943bcce5608cbb886f6fb6
patilur <utkarshapatil0145@gmail.com>
2026-02-26 14:22:38 +0530

    Request response

--- Request_Response/app.js
+++ Request_Response/app.js
@@ -1,24 +1,30 @@
-const http=require('http');
+const http = require('http');
 
-const server=http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     console.log("server is created");
 
     //req.url,req.headers
-    res.setHeader('Content-Type','text/html');
+    res.setHeader('Content-Type', 'text/html');
 
-    if(req.url=='/'){
-        res.statusCode=200; //Ok
-        res.end("<h1>Hello World</h1>")
-    }else if(req.url=='/pizza'){
-        res.statusCode=200; //Ok
-        res.end("<h1>This is your pizza</h1>")
-    }else{
-        res.statusCode=404; //not found
-        res.end("<h1>Page not found</h1>");
+    if (req.url === '/home') {
+        res.statusCode = 200;
+        res.end("<h1>Welcome home</h1>");
+
+    } else if (req.url === '/about') {
+        res.statusCode = 200;
+        res.end("<h1>Welcome to About Us</h1>");
+
+    } else if (req.url === '/node') {
+        res.statusCode = 200;
+        res.end("<h1>Welcome to my Node Js project</h1>");
+
+    } else {
+        res.statusCode = 404;
+        res.end("<h1>Page Not Found</h1>");
     }
 })
 
-let port=3000;
-server.listen(port,()=>{
+let port = 3000;
+server.listen(port, () => {
     console.log("server is runnning");
 })
